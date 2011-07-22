69c7da2e51868d3785df344780572a4413f58f97
Justin Azoff <justin@bouncybouncy.net>
2011-07-22 16:36:45 -0400

    fix package data

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ setup(name='ninfo-plugins',
     version='0.1.0',
     zip_safe=False,
     packages = find_packages(exclude=["tests"]),
+    include_package_data=True,
     install_requires=[
         "ninfo>=0.1.0",
     ],
     entry_points = {
         'ninfo.plugin': [
             'nfi_stats = ninfo_plugins.nfi_stats.nfi_stats_plugin',
+            'siteadvisor = ninfo_plugins.siteadvisor_check.siteadvisor_plugin',
         ]
     }
 ) 
