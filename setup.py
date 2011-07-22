266a0abff1b799212049926824fc24128896a0ec
Justin Azoff <justin@bouncybouncy.net>
2011-07-22 12:16:38 -0400

    use find packages

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
+from setuptools import find_packages
 from glob import glob
 
 setup(name='ninfo-plugins',
     version='0.1.0',
     zip_safe=False,
-    packages = ['ninfo_plugins'],
+    packages = find_packages(exclude=["tests"]),
     package_data = { '': ['*.mako'] },
     install_requires=[
         "ninfo>=0.1.0",
