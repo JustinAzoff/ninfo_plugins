d1f7fd73677bf1077b9a9746bfc0b83ae1997f25
Justin Azoff <justin@bouncybouncy.net>
2011-07-26 13:37:57 -0400

    ldap plugin

---
+++ ninfo_plugins/ldap/ldap_plugin.py
@@ -0,0 +1,41 @@
+from ninfo import PluginBase
+
+class ldap_plugin(PluginBase):
+    """This plugin looks up a user in ldap and returns their information"""
+
+    name = "ldap"
+    title = "LDAP"
+    description = "LDAP Lookup"
+    cache_timeout = 60*60
+    types = ['username']
+
+    def setup(self):
+        c = self.plugin_config
+        import ldap
+        self.ldap = ldap
+        ldap_user   = c['user']
+        ldap_pw     = c['pw']
+        server      = c['server']
+        dsn         = c['dsn']
+        ignore_cert = 'ignore_cert' in c
+        if ignore_cert:
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, 0)
+
+        self.l = ldap.initialize(server)
+        username = 'uid=%s,%s' % (ldap_user, dsn)
+        self.l.simple_bind_s(username, ldap_pw)
+        self.dsn = dsn
+
+    def get_info(self, arg):
+        search = "uid=%s" % arg
+        res = self.l.search_s(self.dsn, self.ldap.SCOPE_SUBTREE, search)
+        res = res[0]
+        key, values = res
+
+        ret = {}
+        for k,v in values.items():
+            ret[k] = ', '.join(v)
+
+        return ret
+
+plugin_class = ldap_plugin
