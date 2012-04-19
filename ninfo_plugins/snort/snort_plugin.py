4ce7e3251d38d3591721c9c15afceee0f51d6fed
Justin Azoff <justin@bouncybouncy.net>
2012-04-19 10:01:42 -0400

    add options support to snort plugin

--- ninfo_plugins/snort/snort_plugin.py
+++ ninfo_plugins/snort/snort_plugin.py
@@ -10,14 +10,19 @@ class snortinfo(PluginBase):
     cache_timeout =  60*5
     types=    ['ip']
 
+    options = { "startdate": {"description": "How far back to search for alerts", "type":"datetime"}}
+
     def setup(self):
         from snort import snortdb
         self.sdb = snortdb.sdb()
         self.sdb.setlimit(100000)
 
-    def get_info(self, ip):
-        time_ago = datetime.date.today() - datetime.timedelta(days=14)
-        time_ago = time_ago.strftime("%Y-%m-%d")
+    def get_info(self, ip, options):
+        if 'startdate' in options:
+            time_ago = options['startdate']
+        else:
+            time_ago = datetime.date.today() - datetime.timedelta(days=14)
+            time_ago = time_ago.strftime("%Y-%m-%d")
         self.sdb.setwhere(startdate=time_ago)
         data = [dict(r) for r in self.sdb.get_events_for_ip(ip)]
         return { 'idsdata': data }
