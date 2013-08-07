98d542353189abb3bcf4aec0577f6d8c493aef3a
Justin Azoff <justin@bouncybouncy.net>
2013-08-07 13:33:00 -0400

    bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ninfo-plugins',
-    version='0.1.10',
+    version='0.1.11',
     zip_safe=False,
     packages = find_packages(exclude=["tests"]),
     include_package_data=True,
